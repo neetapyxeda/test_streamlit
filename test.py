c22285f63cb4ea91822cd1b697565f13ffb28109
neetapyxeda <neeta.alex@pyxeda.ai>
2025-07-31 13:10:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.write("Hello, Please enter a number and I will tell its square..")
 num = st.text_input("Hello, Please enter a number and I will tell its square..")
+num = int(num)
 out = num*num
 st.write("The square value is ",out)
